bc3951e9699cb2e2fd1a00b27ede202978d7ca35
Alex-region <alex_yasinsky@mail.com>
2022-04-25 15:37:01 +0300

    task_4 hard pagination in progress

--- src/store/modules/payments.js
+++ src/store/modules/payments.js
@@ -1,4 +1,20 @@
-
+const db = {
+  "page1": [ 
+    { "id": 1, "date": "20.03.2020", "category": "Food", "value": 169 }, 
+    { "id": 2, "date": "21.03.2020", "category": "Navigation", "value": 50 }, 
+    { "id": 3, "date": "22.03.2020", "category": "Sport", "value": 450 } 
+  ], 
+  "page2": [ 
+      { "id": 4, "date": "23.03.2020", "category": "Entertaiment", "value": 969 }, 
+      { "id": 5, "date": "24.03.2020", "category": "Education", "value": 1500 }, 
+      { "id": 6, "date": "25.03.2020", "category": "Food", "value": 200 }
+  ],
+  "page3": [ 
+    { "id": 7, "date": "23.03.2020", "category": "Entertaiment", "value": 969 }, 
+    { "id": 8, "date": "24.03.2020", "category": "Education", "value": 1500 }, 
+    { "id": 9, "date": "25.03.2020", "category": "Food", "value": 200 } 
+  ],
+}
 
 export default {
 
@@ -25,19 +41,12 @@ export default {
   },
 
   actions: {
-    fetchData({commit}) {
+    fetchData({commit}, pageNumber=1) {
       return new Promise((resolve)=>{
         setTimeout(()=>{
-          const items = []
-          for(let i=1; i<=3; i++) {
-            items.push({
-              date: "23.12.2022",
-              category: "Sport",
-              value: i,
-              id: Math.floor(Math.random()* Math.floor(Math.random() * Date.now()) +i)
-            })
-          } resolve(items)
-        },2000)
+          const page = 'page' + pageNumber;
+          resolve(db[page]);
+        },1000)
       }).then(res=> {
         commit('setPaymentsListData', res)
       })
